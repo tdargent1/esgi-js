9b82158c93c4002d18740914f93f15505b60eb44
tdargent1 <tdargent1@gmail.com>
2020-10-23 17:34:49 +0200

    Edit function VIG

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -96,16 +96,29 @@ function yoda(text){
 }
 yoda("Hello world");
 
-function vig(text, code){
-    if(typeof text !== "string" || text === "")
-        return "";
-
-    while(code.length < text.length){
-        code += code;
+function vig(string, code) {
+    if (typeof string !== "string") return "";
+    if (string.length === 0) return string;
+  
+    while (code.length < string.length) {
+      code += code;
     }
-
-    code = code.substr(0, text.length);
+    code = code.substr(0, string.length);
     let codeIndex = 0;
-
-    return text.split();
+  
+    return string
+      .split("")
+      .map((letter, index) => {
+        letter = letter.toLowerCase();
+        const aCode = "a".charCodeAt(0);
+        const letterNumber = letter.charCodeAt(0) - aCode;
+  
+        if (letterNumber < 0 || letterNumber > 25) return letter;
+  
+        const codeNumber = code.charCodeAt(codeIndex) - aCode;
+        codeIndex++;
+  
+        return String.fromCharCode(((letterNumber + codeNumber) % 26) + aCode);
+      })
+      .join("");
 }
