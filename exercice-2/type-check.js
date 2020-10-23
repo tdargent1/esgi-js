59251835d23f3b0df49710180b4d79a3545d3a75
tdargent1 <tdargent1@gmail.com>
2020-10-23 18:38:23 +0200

    ADD Début type_check_v2

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -14,6 +14,26 @@ function type_check_v1(val, type) {
     }
 }
 
+function type_check_v2(arg1, conf) {
+    if(conf["type"] !== undefined) {
+        if(typeof arg1 !== conf["type"]) {
+            return false;
+        } 
+    }
+    if(conf["value"] !== undefined) {
+        if(arg1 !== conf["value"]) {
+            return false;
+        }
+    }
+    if(conf["enum"] !== undefined) {
+        if(!(arg1 in conf["enum"])) {
+            return false;
+        } 
+    }
+
+    return true;
+}
+
 /* Vérification pour la fonction type_check_v1 */
 console.log(type_check_v1(1, "number"));
 console.log(type_check_v1(1, "string"));
@@ -21,3 +41,7 @@ console.log(type_check_v1("2", "string"));
 console.log(type_check_v1([1, 2], "array"));
 
 /* Vérification pour la fonction type_check_v2 */
+console.log(type_check_v2({prop1 : 1}, {type :  "object"}));
+console.log(type_check_v2("foo", {type : "string", value : "foo"}));
+console.log(type_check_v2("bar", {type : "string", value : "foo"}));
+console.log(type_check_v2(3, {enum : ["foo", "bar", 3]}));
